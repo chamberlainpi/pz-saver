ae99d226144221b08aa8838973c68308fca4a19a
Pierre Chamberlain <chamberlainpi@gmail.com>
2022-03-05 00:16:32 -0400

    Spicing up the UI

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div class="vbox bg-gray-400 w-screen h-screen">
+  <div class="main-app vbox bg-gray-400 w-screen h-screen overflow-y-auto">
+    <div class="abs inset-0 scanlines from-transparent to-black opacity-30 z-0"></div>
+
     <div class="rel w-72 h-32 self-center">
       <img class="logo btn" alt="PZ logo" src="./assets/PzLogo.png" @click="shake('.logo', 0.5, 10)" />
     </div>
 
-    <div class="vbox p-4">
-      <div class="vbox max-w-xl nowrap">
-        <div class="hbox items-center gap-2">
-          <i>PZ Root:</i>
-          <input
-            class="rounded-md p-2 shadow-inner shadow-red-300 bg-opacity-70 bg-white w-full"
-            type="text"
-            v-model="configuration.pzRoot" />
+    <div class="vbox rel z-10 bg-white bg-opacity-80">
+      <div class="vbox p-4">
+        <div class="vbox max-w-xl nowrap">
+          <div class="hbox items-center gap-2">
+            <i>PZ Root:</i>
+            <input
+              class="rounded-md p-2 shadow-inner shadow-red-300 bg-opacity-70 bg-white w-full"
+              type="text"
+              v-model="configuration.pzRoot" />
 
-          <button class="btn bg-green-800 text-white w-48" @click="setConfig">
-            <icon name="save mr-2" />
-            Save
-          </button>
+            <button class="btn bg-green-800 text-white w-48" @click="setConfig">
+              <icon name="save mr-2" />
+              Save
+            </button>
+          </div>
         </div>
+
+        <template v-if="configuration.pzRoot">
+          <PanelConfigGameFolders ref="panelConfigGameFolders" :config="configuration" />
+
+          <PanelFileDiffs ref="panelFileDiffs" @save-snapshot="onSnapshotSaved" />
+
+          <PanelSavedSnapshots ref="panelSavedSnapshots" />
+        </template>
       </div>
-
-      <template v-if="configuration.pzRoot">
-        <PanelConfigGameFolders ref="panelConfigGameFolders" :config="configuration" />
-
-        <PanelFileDiffs ref="panelFileDiffs" @save-snapshot="onSnapshotSaved" />
-
-        <PanelSavedSnapshots ref="panelSavedSnapshots" />
-      </template>
     </div>
   </div>
 </template>
@@ -48,4 +52,8 @@ const panelSavedSnapshots = ref(null)
 async function onSnapshotSaved() {
   await panelSavedSnapshots.value.updateBackupsList()
 }
+
+onMounted(() => {
+  gsap.to('.scanlines', 0.45, { '--scanline-y': '5px', repeat: -1, ease: t => t })
+})
 </script>
